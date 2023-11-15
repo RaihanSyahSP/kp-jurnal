e201ac0c8a07fe020395245c1521694e74825f8f
Raihan Syah <raihansyah45@gmail.com>
2023-11-15 10:53:35 +0700

    feat:add download excel

--- app/views/gscholar/index.php
+++ app/views/gscholar/index.php
@@ -51,11 +51,60 @@
 
 
      $(document).ready(function() {
+
+         // function for export all data excel with server side processing
+         function newexportaction(e, dt, button, config) {
+             var self = this;
+             var oldStart = dt.settings()[0]._iDisplayStart;
+             dt.one('preXhr', function(e, s, data) {
+                 // Just this once, load all data from the server...
+                 data.start = 0;
+                 data.length = 2147483647;
+                 dt.one('preDraw', function(e, settings) {
+                     // Call the original action function
+                     if (button[0].className.indexOf('buttons-copy') >= 0) {
+                         $.fn.dataTable.ext.buttons.copyHtml5.action.call(self, e, dt, button, config);
+                     } else if (button[0].className.indexOf('buttons-excel') >= 0) {
+                         $.fn.dataTable.ext.buttons.excelHtml5.available(dt, config) ?
+                             $.fn.dataTable.ext.buttons.excelHtml5.action.call(self, e, dt, button, config) :
+                             $.fn.dataTable.ext.buttons.excelFlash.action.call(self, e, dt, button, config);
+                     } else if (button[0].className.indexOf('buttons-csv') >= 0) {
+                         $.fn.dataTable.ext.buttons.csvHtml5.available(dt, config) ?
+                             $.fn.dataTable.ext.buttons.csvHtml5.action.call(self, e, dt, button, config) :
+                             $.fn.dataTable.ext.buttons.csvFlash.action.call(self, e, dt, button, config);
+                     } else if (button[0].className.indexOf('buttons-pdf') >= 0) {
+                         $.fn.dataTable.ext.buttons.pdfHtml5.available(dt, config) ?
+                             $.fn.dataTable.ext.buttons.pdfHtml5.action.call(self, e, dt, button, config) :
+                             $.fn.dataTable.ext.buttons.pdfFlash.action.call(self, e, dt, button, config);
+                     } else if (button[0].className.indexOf('buttons-print') >= 0) {
+                         $.fn.dataTable.ext.buttons.print.action(e, dt, button, config);
+                     }
+                     dt.one('preXhr', function(e, s, data) {
+                         // DataTables thinks the first item displayed is index 0, but we're not drawing that.
+                         // Set the property to what it was before exporting.
+                         settings._iDisplayStart = oldStart;
+                         data.start = oldStart;
+                     });
+                     // Reload the grid with the original page. Otherwise, API functions like table.cell(this) don't work properly.
+                     setTimeout(dt.ajax.reload, 0);
+                     // Prevent rendering of the full data to the DOM
+                     return false;
+                 });
+             });
+             // Requery the server with the new one-time export settings
+             dt.ajax.reload();
+         }
+
          $('#myTable').DataTable({
-             //  dom: 'Bfrtip',
-             //  buttons: [
-             //      'copy', 'csv', 'excel', 'pdf', 'print'
-             //  ],
+             "dom": 'Blfrtip',
+             "buttons": [
+                {
+                 "extend": 'excel',
+                 "text": '<button class="btn btn-outline absolute left-40">Download Excel</button>',
+                 "titleAttr": 'Excel',
+                 "action": newexportaction
+                },
+            ],
              "processing": true,
              "serverSide": true,
              "pageLength": 10,
