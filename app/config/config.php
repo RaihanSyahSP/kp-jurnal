65a77784ae9a1e79d0b6bce9b082fba4fe04f222
Raihan Syah <raihansyah45@gmail.com>
2023-11-20 13:52:13 +0700

    fix

--- app/config/config.php
+++ app/config/config.php
@@ -5,5 +5,5 @@ define('BASEURL', 'https://jpi.unikom.ac.id');
 // DB
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'BisaBisa474!');
+define('DB_PASS', '');
 define('DB_NAME', 'sinta');
